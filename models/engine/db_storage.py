10ef6a00081342cd8cfc992215fe4783113ca24e
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2023-12-28 14:54:07 +0000

    task_7

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -78,3 +78,7 @@ class DBStorage:
         Base.metadata.create_all(self.__engine)
         Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
         self.__session = scoped_session(Session)
+
+    def close(self):
+        """It cleans up a session in a thread."""
+        self.__session.remove()
