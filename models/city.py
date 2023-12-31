e0a79bd505d4f015b740a1331df3561ade460e1c
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2023-12-31 15:41:55 +0000

    city model

--- models/city.py
+++ models/city.py
@@ -3,10 +3,12 @@
 from models.base_model import BaseModel
 from models.base_model import Base
 from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
-    state_id = ""
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    state = relationship("State", back_populates="cities")
